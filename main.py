59f326ed21f1e837209a0d33974a5210811c4d3e
ForeverTrainee <passxbox33@gmail.com>
2023-12-06 17:55:55 +0100

    Made  API keys as environment variable

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import time
+<<<<<<< HEAD
 
 import requests
 import datetime
 
+=======
+import os
+import requests
+import datetime
+
+
+>>>>>>> 5ff739f (Made  API keys as environment variable)
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
@@ -10,8 +18,13 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 
+<<<<<<< HEAD
 ALPHA_API_KEY = "Z6APOLEE9VA1B53D"
 NEWS_API_KEY = "992b893efece4269989d5c355bc1dca9"
+=======
+ALPHA_API_KEY = os.getenv('ALPHA_API_KEY')
+NEWS_API_KEY = os.getenv('NEWS_API_KEY')
+>>>>>>> 5ff739f (Made  API keys as environment variable)
 
 
 stock_params = {
@@ -29,12 +42,21 @@ print(yesterday_data)
 print(yesterday_closing_price)
 # Get the day before yesterday's closing stock price
 
+<<<<<<< HEAD
 day_before_yestarday_data = data_list[1]
 day_before_yestarday_closing_price = day_before_yestarday_data["4. close"]
 print(day_before_yestarday_closing_price)
 
 # Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
 difference = abs(float(yesterday_closing_price) - float(day_before_yestarday_closing_price))
+=======
+day_before_yesterday_data = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
+print(day_before_yesterday_closing_price)
+
+# Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
+difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
+>>>>>>> 5ff739f (Made  API keys as environment variable)
 print(difference)
 
 # Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
@@ -43,7 +65,11 @@ print(diff_percent)
 
 # If percentage is greater than 5 then print("Get News").
 # Use the News API to get articles related to the COMPANY_NAME.
+<<<<<<< HEAD
 if diff_percent > 1:
+=======
+if diff_percent > 5:
+>>>>>>> 5ff739f (Made  API keys as environment variable)
     news_params = {
         "apiKey":NEWS_API_KEY,
         "qInTitle": COMPANY_NAME,
@@ -51,7 +77,11 @@ if diff_percent > 1:
     news_response = requests.get(NEWS_ENDPOINT,params=news_params)
     articles = news_response.json()["articles"]
     three_articles = articles[:3]
+<<<<<<< HEAD
     formatted_articles =[f"Headline: {article['title']}.\n Brief: {article['description']}" for article in three_articles]
+=======
+    formatted_articles = [f"Headline: {article['title']}.\n Brief: {article['description']}" for article in three_articles]
+>>>>>>> 5ff739f (Made  API keys as environment variable)
 
 for article in formatted_articles:
     print(article)
